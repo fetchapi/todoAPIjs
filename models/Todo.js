38656a8a6b47653a52e5378dc9531ef9829a92a4
huynhit24 <huynh.it.24@gmail.com>
2022-12-19 21:35:34 +0700

    add field status in Todo model

--- models/Todo.js
+++ models/Todo.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var TodoSchema = new mongoose.Schema({
   name: String,
   completed: Boolean,
-  note: String
+  note: String,
+  status: Boolean
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
